87b9dcf7f897ac30deef9cdcf2920b887b0c3c2c
Amunratis <sirthesurfer@gmail.com>
2018-02-27 17:48:23 +0100

    My own implementation of Club Sandwich project for Nano Degree program

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -3,7 +3,10 @@ package com.udacity.sandwichclub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
@@ -15,12 +18,32 @@ public class DetailActivity extends AppCompatActivity {
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
 
+
+    public TextView alsoKnown;
+    public TextView origin ;
+    public TextView ingredients ;
+    public TextView mDescription ;
+    public TextView mAlsoKnownLabel ;
+    public TextView mOriginLabel ;
+    public TextView mIngredientsLabel ;
+    public ImageView imageView;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        ImageView ingredientsIv = findViewById(R.id.image_iv);
+        imageView = findViewById(R.id.image_iv);
+
+        alsoKnown = findViewById(R.id.also_known);
+        origin = findViewById(R.id.origin_tv);
+        ingredients = findViewById(R.id.ingredients_tv);
+        mDescription = findViewById(R.id.description_tv);
+        mAlsoKnownLabel = findViewById(R.id.also_known_as_label);
+        mOriginLabel = findViewById(R.id.detail_place_of_origin_label);
+        mIngredientsLabel = findViewById(R.id.detail_ingredients_label);
+
 
         Intent intent = getIntent();
         if (intent == null) {
@@ -28,6 +51,7 @@ public class DetailActivity extends AppCompatActivity {
         }
 
         int position = intent.getIntExtra(EXTRA_POSITION, DEFAULT_POSITION);
+
         if (position == DEFAULT_POSITION) {
             // EXTRA_POSITION not found in intent
             closeOnError();
@@ -42,11 +66,12 @@ public class DetailActivity extends AppCompatActivity {
             closeOnError();
             return;
         }
+        //Populate sandwich object that's used by populateUI
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
-                .into(ingredientsIv);
+                .into(imageView);
 
         setTitle(sandwich.getMainName());
     }
@@ -56,7 +81,29 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich sandwich) {
+
+        if(sandwich.getIngredients()!=null) {
+            ingredients.setText(TextUtils.join(" ", sandwich.getIngredients()));
+        }else {
+            ingredients.setText("no data");
+        }
+        mDescription.setText(sandwich.getDescription());
+
+        //Display AlsoKnownAs details or else hide it
+        if(!TextUtils.join(", ", sandwich.getAlsoKnownAs()).equals("")){
+        alsoKnown.setText(TextUtils.join(", ", sandwich.getAlsoKnownAs()));
+        } else {
+            mAlsoKnownLabel.setVisibility(View.GONE);
+            alsoKnown.setVisibility(View.GONE);
+        }
+
+
+        if (!sandwich.getPlaceOfOrigin().equals("")) {
+            origin.setText(sandwich.getPlaceOfOrigin());
+        } else {
+            origin.setText("Unknown");
+        }
 
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,10 +1,68 @@
 package com.udacity.sandwichclub.utils;
 
+import android.text.TextUtils;
+import android.util.Log;
+
 import com.udacity.sandwichclub.model.Sandwich;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class JsonUtils {
 
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+
+        if (TextUtils.isEmpty(json)) {
+            return null;
+        }
+
+        String mainName;
+        List<String> alsoKnownAs = new ArrayList<>();
+        String placeOfOrigin;
+        String description;
+        String image;
+        List<String> ingredients = new ArrayList<>();
+        Sandwich sandwich=new Sandwich();
+
+        try {
+            // Create a JSONObject from the JSON response string
+            JSONObject baseJsonResponse = new JSONObject(json);
+            JSONObject sandwichName = baseJsonResponse.getJSONObject("name");
+            mainName=sandwichName.optString("mainName");
+            sandwich.setMainName(mainName);
+
+            JSONArray jsonArrayNames=sandwichName.getJSONArray("alsoKnownAs");
+
+            alsoKnownAs.add(jsonArrayNames.join(" "));
+            sandwich.setAlsoKnownAs(alsoKnownAs);
+
+            placeOfOrigin=baseJsonResponse.optString("placeOfOrigin");
+            sandwich.setPlaceOfOrigin(placeOfOrigin);
+
+            description=baseJsonResponse.optString("description");
+            sandwich.setDescription(description);
+
+            image=baseJsonResponse.optString("image");
+            sandwich.setImage(image);
+
+            JSONArray jsonArrayIngredients=baseJsonResponse.getJSONArray("ingredients");
+
+            ingredients.add(jsonArrayIngredients.join(","));
+
+            sandwich.setIngredients(ingredients);
+
+        } catch (JSONException e) {
+            // If an error is thrown when executing any of the above statements in the "try" block,
+            // catch the exception here, so the app doesn't crash. Print a log message
+            // with the message from the exception.
+            Log.e("QueryUtils", "Problem parsing the sandwich JSON results", e);
+        }
+
+
+        return sandwich;
     }
 }
